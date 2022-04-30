59d20aa4ac778732a8c24b6ef00afa7360ab27c8
pdmnhn <dev.pdmnhn@gmail.com>
2022-04-30 17:02:58 +0530

    Add a message to default route

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import accountRouter from "./controllers/account";
 import toolsRouter from "./controllers/tools";
 import { tokenExtractor, userExtractor } from "./utils/middleware";
 
 const app = express();
 app.use(express.json());
+
+app.get("/", (_req: Request, res: Response) => {
+  res.status(200).json({
+    status: "This application is deployed by EIE students for a DBMS project",
+  });
+});
 
 app.use("/api/account", accountRouter);
 
