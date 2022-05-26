88ccd979ff65616d57d3f5b725eb9950142a4b91
pdmnhn <dev.pdmnhn@gmail.com>
2022-05-26 23:13:36 +0530

    Change formatting

--- src/database/cursors.sql
+++ src/database/cursors.sql
@@ -1,27 +1,20 @@
 CREATE FUNCTION get_all_email()
 	RETURNS TEXT AS $$
 DECLARE
-
 emails TEXT DEFAULT '';
 cur_users CURSOR
 		  FOR SELECT email
 		  FROM users;
 email users.email%TYPE;
-
 BEGIN
 OPEN cur_users;
-	
 LOOP
 	FETCH cur_users INTO email;
 	EXIT WHEN NOT FOUND;
 	
 	emails := emails || email || ', ';
-
 END LOOP;
-	
 CLOSE cur_users;
-
 RETURN emails;
 END; $$
-
 LANGUAGE PLPGSQL;
