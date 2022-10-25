8eb98872b270d69730885c7c3a81d4db94e523e6
Padmanabhan <dev.pdmnhn@gmail.com>
2022-10-25 21:14:45 +0530

    Update DB sample data

--- src/database/sample_data.sql
+++ src/database/sample_data.sql
@@ -1,36 +1,23 @@
-INSERT INTO
-    brands (brand_id, brand_name)
-VALUES
-    (1, 'Fluke'),
+INSERT INTO brands (brand_id, brand_name)
+VALUES (1, 'Fluke'),
     (2, 'Siemens'),
     (3, 'Emerson'),
     (4, 'Mitsubishi');
-
-INSERT INTO
-    categories (category_id, category_name)
-VALUES
-    (1, 'Electrical'),
+INSERT INTO categories (category_id, category_name)
+VALUES (1, 'Electrical'),
     (2, 'Mechanical'),
     (3, 'Electronics');
-
-INSERT INTO
-    tool_types (tooltype_id, tooltype_name, category_id)
-VALUES
-    (1, 'Drilling Machine', 1),
+INSERT INTO tool_types (tooltype_id, tooltype_name, category_id)
+VALUES (1, 'Drilling Machine', 1),
     (2, 'Motor', 2),
     (3, 'LASER Cutter', 3),
     (4, 'Electrode', 1);
-
-INSERT INTO
-    tool_subtypes (subtype_id, tooltype_id, subtype_name)
-VALUES
-(1, 1, 'Portable'),
-    (2, 2, 'DC'),
-    (3, 3, 'For Fabric'),
-    (4, 4, 'Welding of 1 square meter');
-
-INSERT INTO
-    tools (
+INSERT INTO tool_subtypes (subtype_id, tooltype_id, subtype_name)
+VALUES (1, 1, 'Handheld'),
+    (2, 2, 'Induction Motor'),
+    (3, 3, 'For Metal'),
+    (4, 4, 'Medium Size');
+INSERT INTO tools (
         tool_code,
         encryption_code,
         subtype_id,
@@ -40,10 +27,9 @@ INSERT INTO
         usage_type,
         last_scan
     )
-VALUES
-    (
+VALUES (
         1,
-        'KDFHKDHFHDFJKAHF',
+        '136424553',
         3,
         1,
         3847,
@@ -53,28 +39,14 @@ VALUES
     ),
     (
         2,
-        'DKFHAKFHKDLKDHDH',
+        '198273146',
         2,
         3,
         7502,
         'good',
         'reusable',
         NOW()
-    ),
-    (
-        3,
-        'QIOEUHDLKFAHFHDF',
-        4,
-        4,
-        3943,
-        'good',
-        'once',
-        NOW()
     );
-
-INSERT INTO
-    scan_log(tool_code, status, taken)
-VALUES
-(1, 'good', false),
-    (2, 'good', false),
-    (3, 'good', false);
+INSERT INTO scan_log(tool_code, status, taken)
+VALUES (1, 'good', false),
+    (2, 'good', false);
