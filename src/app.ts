e0a05ef5f2e0a27ec13c8e50b29b999ed5772f51
Padmanabhan <dev.pdmnhn@gmail.com>
2022-12-09 21:24:00 +0530

    Add CORS

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,12 @@ import express, { Request, Response } from "express";
 import accountRouter from "./controllers/account";
 import toolsRouter from "./controllers/tools";
 import { tokenExtractor, userExtractor } from "./utils/middleware";
+import cors from "cors";
 
 const app = express();
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (_req: Request, res: Response) => {
